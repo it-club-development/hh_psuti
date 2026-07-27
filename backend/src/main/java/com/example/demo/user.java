e253671bd67e2e9a40becc81c109cc19ac039f9a
Chipolino11 <ruslick11@mail.ru>
2026-07-27 11:29:22 +0400

    1

--- backend/src/main/java/com/example/demo/user.java
+++ backend/src/main/java/com/example/demo/user.java
@@ -3,4 +3,23 @@ package com.example.demo;
 public class user {
     private String email;
     private String password;
+
+    public user() {}
+
+    public user(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
