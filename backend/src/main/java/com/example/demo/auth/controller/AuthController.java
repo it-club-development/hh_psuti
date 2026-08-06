222d0db57e04169dd366839ea767ac4805a4079c
Chipolino11 <ruslick11@mail.ru>
2026-08-06 17:42:22 +0400

    Update AuthController.java

--- backend/src/main/java/com/example/demo/auth/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/auth/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class AuthController {
     @Autowired
